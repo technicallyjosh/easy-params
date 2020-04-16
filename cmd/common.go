2143c62c06f74f61a8640d8ac50a8a10818a5705
Josh Newman <git@technicallyjosh.com>
2020-04-16 16:41:56 -0600

    add string chunks helper function

--- cmd/common.go
+++ cmd/common.go
@@ -1,8 +1,12 @@
 package cmd
 
 import (
-	"github.com/jedib0t/go-pretty/table"
+	"sort"
+	"strings"
 	"time"
+
+	"github.com/aws/aws-sdk-go/service/ssm"
+	"github.com/jedib0t/go-pretty/table"
 )
 
 func formatDate(dt *time.Time) string {
@@ -22,4 +26,21 @@ func insertColumn(row table.Row, index int, item interface{}) table.Row {
 	row[index] = item
 
 	return row
+}
+
+func getStringChunks(arr []*string, batchSize int) [][]*string {
+	batches := make([][]*string, 0, (len(arr)+batchSize-1)/batchSize)
+
+	for batchSize < len(arr) {
+		arr, batches = arr[batchSize:], append(batches, arr[0:batchSize:batchSize])
+	}
+	batches = append(batches, arr)
+
+	return batches
+}
+
+func sortParams(params []*ssm.Parameter) {
+	sort.Slice(params, func(i, j int) bool {
+		return strings.ToLower(*params[i].Name) < strings.ToLower(*params[j].Name)
+	})
 }
